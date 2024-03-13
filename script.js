59f73fd2d52d1201d7fbc863d66b0d99e0beefb2
Emilie Layani <emilielayani@gmail.com>
2024-03-13 15:52:55 -0400

    display quote in dom

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let apiQuotes = {}
 
+const quoteText = document.getElementById('quote-text')
+
 async function getQuote() {
     try {
         let response = await fetch('./data.json');
-        apiQuotes = await response.json()
-        console.log('Success:', JSON.stringify(apiQuotes, undefined, 2))
+        apiQuotes = await response.json();
+        let random = Math.floor(Math.random() * (28 - 1) + 1)
+        console.log(apiQuotes[random])
+        quoteText.textContent = apiQuotes[random]
     } catch (e) {
         console.log('Err in getQuote', e)
     }
 }
 
+// onLoad
 getQuote()
